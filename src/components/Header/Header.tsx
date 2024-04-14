e201cef5d8c5d695cb2436f9d63b6fc559f5aaa2
martnemov <mr.nemov@gmail.com>
2024-04-14 17:12:14 +0500

    Fix Home layout, add Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,20 +3,18 @@ import style from './Header.module.css'
 function Header() {
     return (
         <div className={style.header}>
-            <div className={style.headerColumn}>
+            <div className={style.headerLogo}>
                 <img src="/src/assets/shared/logo.svg" alt="logo"/>
-                <hr/>
+                <div className={style.stripe}></div>
             </div>
-            <div className={style.headerNav}>
-                <nav>
-                    <ul>
+            <nav>
+            <ul>
                         <li>00 HOME</li>
                         <li>01 DESTINATION</li>
                         <li>02 CREW</li>
                         <li>03 TECHNOLOGY</li>
                     </ul>
                 </nav>
-            </div>
         </div>
     )
 }
