c8bc76e2ac971bac1129c030fe210efb4a9168a5
shtirlizc <shtirlizc@gmail.com>
2024-04-15 13:32:14 +0500

    markup update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,20 +2,20 @@ import style from './Header.module.css'
 
 function Header() {
     return (
-        <div className={style.header}>
+        <header className={style.header}>
             <div className={style.headerLogo}>
                 <img src="/src/assets/shared/logo.svg" alt="logo"/>
-                <div className={style.stripe}></div>
             </div>
+            <div className={style.stripe}></div>
             <nav>
-            <ul>
-                        <li>00 HOME</li>
-                        <li>01 DESTINATION</li>
-                        <li>02 CREW</li>
-                        <li>03 TECHNOLOGY</li>
-                    </ul>
-                </nav>
-        </div>
+                <ul>
+                    <li><a href="#">00 Home</a></li>
+                    <li><a href="#">01 DESTINATION</a></li>
+                    <li><a href="#">02 Crew</a></li>
+                    <li><a href="#">03 TECHNOLOGY</a></li>
+                </ul>
+             </nav>
+        </header>
     )
 }
 
