78e87f19d0c27234a4c0d6dab88fa1bb3a62fb6e
shtirlizc <shtirlizc@gmail.com>
2024-04-15 13:39:26 +0500

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import './App.css'
-import Home from "./components/Home/Home.tsx";
-import Header from "./components/Header/Header.tsx";
+import './App.css';
+import Home from './components/Home/Home.tsx';
+import Header from './components/Header/Header.tsx';
 
 function App() {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <div className='home-wrapper'>
-                <Home/>
-                <div className='home-bg'></div>
+        <div className="app-wrapper">
+            <Header />
+            <div className="home-wrapper">
+                <Home />
+                <div className="home-bg"></div>
             </div>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,22 @@
-import style from './Header.module.css'
+import style from './Header.module.css';
 
 function Header() {
     return (
         <div className={style.header}>
             <div className={style.headerLogo}>
-                <img src="/src/assets/shared/logo.svg" alt="logo"/>
+                <img src="/src/assets/shared/logo.svg" alt="logo" />
                 <div className={style.stripe}></div>
             </div>
             <nav>
-            <ul>
-                        <li>00 HOME</li>
-                        <li>01 DESTINATION</li>
-                        <li>02 CREW</li>
-                        <li>03 TECHNOLOGY</li>
-                    </ul>
-                </nav>
+                <ul>
+                    <li>00 HOME</li>
+                    <li>01 DESTINATION</li>
+                    <li>02 CREW</li>
+                    <li>03 TECHNOLOGY</li>
+                </ul>
+            </nav>
         </div>
-    )
+    );
 }
 
 export default Header;
