3ea53245176204bc465eafed60c9e75bd5eeab34
Mart Nemov <mr.nemov@gmail.com>
2026-05-25 15:18:37 +0500

    refactor: update Header component structure and formatting adjustments

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,26 +4,38 @@ function Header() {
     return (
         <header className={style.header}>
             <div className={style.headerLogo}>
-                <img src="/src/assets/shared/logo.svg" alt="logo"/>
+                <img src="/src/assets/shared/logo.svg" alt="logo" />
             </div>
             <div className={style.stripe}></div>
             <nav>
                 <ul>
-                    <li><a href="#">00 Home</a></li>
-                    <li><a href="#">01 DESTINATION</a></li>
-                    <li><a href="#">02 Crew</a></li>
-                    <li><a href="#">03 TECHNOLOGY</a></li>
-                </ul>
-             </nav>
-        </header>
-    )
-                    <li>00 HOME</li>
-                    <li>01 DESTINATION</li>
-                    <li>02 CREW</li>
-                    <li>03 TECHNOLOGY</li>
+                    <li>
+                        <a href="#">00 Home</a>
+                    </li>
+                    <li>
+                        <a href="#">01 DESTINATION</a>
+                    </li>
+                    <li>
+                        <a href="#">02 Crew</a>
+                    </li>
+                    <li>
+                        <a href="#">03 TECHNOLOGY</a>
+                    </li>
                 </ul>
             </nav>
-        </div>
+
+            <div>
+                <nav>
+                    <ul>
+                        <li>00 HOME</li>
+                        <li>01 DESTINATION</li>
+                        <li>02 CREW</li>
+                        <li>03 TECHNOLOGY</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     );
+}
 
 export default Header;
